911fb8996d501d40142549b42c63778fb8fda974
HenriqueZapellaRochagit config --global user.email henri_zap@hotmail.com <henri_zap@hotmail.com>
2023-08-14 15:36:52 -0300

    first commit

---
+++ LinkedList/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct LinkedList
+{
+    int numero;
+    struct LinkedList *proximo;
+    int tamanho;
+};
+
+typedef struct LinkedList list;
+
+list *primeiroNodo;
+
+void add(int x) {
+    
+    list *lista = (list*) malloc(sizeof(list));
+    lista -> numero = x;
+    lista -> proximo = primeiroNodo;
+    primeiroNodo = lista;
+}
+
+
+int main(){
+
+    add(1);
+    add(2);
+    add(3);
+    add(4);
+    add(5);
+
+
+}
