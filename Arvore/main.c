b28f70c0b78e983a82dded0ad6cbacf3eec7a586
HenriqueZapellaRochagit config --global user.email henri_zap@hotmail.com <henri_zap@hotmail.com>
2023-08-18 09:09:52 -0300

    tests added, apparently its run ok

--- Arvore/main.c
+++ Arvore/main.c
@@ -15,7 +15,7 @@ int nElemntos=0;
 Tree *root;
 
 
-bool add(int value, Tree node) {
+bool add(int value, Tree *node) {
 
     // case the root dont exist
     if (root == NULL) {
@@ -28,35 +28,35 @@ bool add(int value, Tree node) {
     }
 
     // if the value is small than the node
-    if(node.value > value) {
+    if(node->value > value) {
     // if the node dont have a left child    
-    if (node.leftNode == NULL){
-        node.leftNode=malloc(sizeof(Tree));
-        node.leftNode->value =value;
-        node.leftNode->leftNode=NULL;
-        node.leftNode->rightNode=NULL;
+    if (node->leftNode == NULL){
+        node->leftNode=malloc(sizeof(Tree));
+        node->leftNode->value =value;
+        node->leftNode->leftNode=NULL;
+        node->leftNode->rightNode=NULL;
         nElemntos++;
         return true;  
     // if the node have a left child. recursive call passing the left node  
     } else {
-        add(value, *node.leftNode);
+        add(value, node->leftNode);
     }  
     // if the value is bigger than the node
-    } else if(node.value < value) {
+    } else if(node->value < value) {
         // if the node dont have a right child    
-        if (node.leftNode == NULL){
-        node.rightNode=malloc(sizeof(Tree));
-        node.rightNode->value =value;
-        node.rightNode->leftNode=NULL;
-        node.rightNode->rightNode=NULL;
+        if (node->rightNode == NULL){
+        node->rightNode=malloc(sizeof(Tree));
+        node->rightNode->value =value;
+        node->rightNode->leftNode=NULL;
+        node->rightNode->rightNode=NULL;
         nElemntos++;   
         return true; 
     // if the node have a right child. recursive call passing the right node 
     } else {
-        add(value, *node.rightNode);
+        add(value, node->rightNode);
     }  
     }
-
+    
     return false;
     }
 
@@ -64,7 +64,14 @@ bool add(int value, Tree node) {
 
 int main(void) {
 
-   
+   add(9,root);
+   add(4, root);
+   add(3, root);
+   add(13, root);
+   add(14, root);
+   add(10, root);
+  
+   printf("%d   %d   %d  //  %d   %d    %d", root->value, root->leftNode->value, root->leftNode->leftNode->value, root->rightNode->value, root->rightNode->rightNode->value, root->rightNode->leftNode->value);
 
     
     return 0;
