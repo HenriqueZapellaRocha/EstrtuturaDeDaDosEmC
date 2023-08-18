2e5e20037701c6b23d4eee2a6f36cb67dc31401c
HenriqueZapellaRochagit config --global user.email henri_zap@hotmail.com <henri_zap@hotmail.com>
2023-08-18 10:45:31 -0300

    search method is added, aparentely its ok

--- Arvore/main.c
+++ Arvore/main.c
@@ -39,7 +39,7 @@ bool add(int value, Tree *node) {
         return true;  
     // if the node have a left child. recursive call passing the left node  
     } else {
-        add(value, node->leftNode);
+    return add(value, node->leftNode);
     }  
     // if the value is bigger than the node
     } else if(node->value < value) {
@@ -53,7 +53,7 @@ bool add(int value, Tree *node) {
         return true; 
     // if the node have a right child. recursive call passing the right node 
     } else {
-        add(value, node->rightNode);
+    return add(value, node->rightNode);
     }  
     }
     
@@ -62,7 +62,31 @@ bool add(int value, Tree *node) {
 
 
 
+bool search(int value, Tree *node) {
+
+    // verify if the actual node is identical value than we are comparing
+    if (node != NULL && node->value == value) {
+        return true;
+    }
+
+    // if the value we are searching is smaller than the node we have, so we make a recursive call passing the left children
+    if (value < node->value && node->leftNode != NULL) {
+        return search(value, node->leftNode);
+    // if the value we are searching is bigger than the node we have, so we make a recursive call passing the right children
+    } else if (value > node->value && node->rightNode != NULL) {
+        return search(value, node->rightNode);
+    }
+
+
+return false;
+
+
+ }      
+
+
+
 int main(void) {
+
 
    add(9,root);
    add(4, root);
@@ -71,8 +95,9 @@ int main(void) {
    add(14, root);
    add(10, root);
   
-   printf("%d   %d   %d  //  %d   %d    %d", root->value, root->leftNode->value, root->leftNode->leftNode->value, root->rightNode->value, root->rightNode->rightNode->value, root->rightNode->leftNode->value);
+   printf("%d   %d   %d  //  %d   %d    %d\n", root->value, root->leftNode->value, root->leftNode->leftNode->value, root->rightNode->value, root->rightNode->rightNode->value, root->rightNode->leftNode->value);
 
+    printf("%d", search(4,root));
     
     return 0;
 }
